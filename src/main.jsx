863ca7ed56a391e8a10bda5a19caba8964571de2
xizhou1204 <xizhou1204@gmail.com>
2026-08-26 15:43:22 +0800

    Use recovered stylesheet entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import '../assets/index-DHBhs0fk.css'
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
